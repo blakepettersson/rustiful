7e8c41d2cda9ef67e154e1280b52e697945c0871
Blake Pettersson <blake.pettersson@gmail.com>
2017-03-13 15:49:57 +0100

    Impl Display + Error
    Add an implementation for Display and std::error::Error for
    QueryStringParseError.

--- jsonapi/src/queryspec.rs
+++ jsonapi/src/queryspec.rs
@@ -1,12 +1,6 @@
-extern crate serde;
-
+use std::fmt::*;
 use std::str::FromStr;
-
-use std::marker::PhantomData;
-use std::collections::HashMap;
-use std::collections::HashSet;
-use self::serde::ser::Serialize;
-use self::serde::de::Deserialize;
+use std::error::Error;
 
 pub trait ToParams {
     type Params: FromStr;
@@ -23,4 +17,39 @@ pub enum QueryStringParseError {
     InvalidValue(String),
     ParseError(String),
     UnImplementedError,
+}
+
+static UNIMPLEMENTED: &'static str = "Unimplemented";
+
+impl Display for QueryStringParseError {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        use self::QueryStringParseError::*;
+
+        let msg = match *self {
+            InvalidParam(ref desc) => desc,
+            InvalidKeyParam(ref desc) => desc,
+            InvalidValue(ref desc) => desc,
+            ParseError(ref desc) => desc,
+            UnImplementedError => UNIMPLEMENTED,
+        };
+        write!(f, "Query string parse error: {}", msg)
+    }
+}
+
+impl Error for QueryStringParseError {
+    fn description(&self) -> &str {
+        use self::QueryStringParseError::*;
+
+        match *self {
+            InvalidParam(ref desc) => desc,
+            InvalidKeyParam(ref desc) => desc,
+            InvalidValue(ref desc) => desc,
+            ParseError(ref desc) => desc,
+            UnImplementedError => UNIMPLEMENTED,
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        None
+    }
 }
