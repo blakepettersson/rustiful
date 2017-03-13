7bb8ef3da9790022b8ae43451bef9df7ff147f9f
Blake Pettersson <blake.pettersson@gmail.com>
2017-03-13 15:51:41 +0100

    Refactor JsonApiService
    Enable the access of type-safe params when finding a given resource.

--- jsonapi/src/service.rs
+++ jsonapi/src/service.rs
@@ -1,72 +1,23 @@
 extern crate serde_json;
 extern crate serde;
 
-use queryspec::ToTypedQuerySpec;
+use queryspec::ToParams;
 use self::serde::ser::Serialize;
 use self::serde::de::Deserialize;
-use super::queryspec::QuerySpec;
 
-pub trait JsonApiService<T, E, S = Self>
-    where T: Serialize + Deserialize  + ToTypedQuerySpec
+pub trait JsonApiService
 {
-    //use hyper::{Get, Post, StatusCode};
-    //use hyper::header::ContentLength;
-    //use hyper::server::{Http, Service, Request, Response};
+    type Error;
+    type T: Serialize + Deserialize + ToParams;
 
-    fn find(id: &str, QuerySpec<T>) -> Result<Option<T>, E>;
+    fn find(&self, id: &str, params: <Self::T as ToParams>::Params) -> Result<Option<Self::T>, Self::Error>;
 
-    fn save(record: T) -> Result<T, E>;
+    fn find_all(&self, params: <Self::T as ToParams>::Params) -> Result<Vec<Self::T>, Self::Error>;
 
-    fn delete(id: &str) -> Result<(), E>;
+    fn save(&self, record: Self::T) -> Result<Self::T, Self::Error>;
+
+    fn delete(&self, id: &str) -> Result<(), Self::Error>;
 }
 
-#[cfg(test)]
-mod tests {
-    use std::collections::HashMap;
-    use super::super::queryspec::QuerySpec;
-    use super::super::queryspec::ToTypedQuerySpec;
-    use super::super::queryspec::QueryStringParseError;
-    use super::super::service::JsonApiService;
+pub trait ToRequest<T> where T: JsonApiService {}
 
-    #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
-    struct Foo {
-        bar: Option<String>,
-    }
-
-    struct Bar {
-        foo: Option<String>
-    }
-
-    struct TestService;
-    impl JsonApiService<Foo, String> for TestService {
-        fn find(id: &str, query_spec: QuerySpec<Foo>) -> Result<Option<Foo>, String> {
-            Ok(Some(Foo { bar: query_spec.fields().foo }))
-        }
-
-        fn save(record: Foo) -> Result<Foo, String> {
-            Ok(Foo { bar: None })
-        }
-
-        fn delete(id: &str) -> Result<(), String> {
-            Ok(())
-        }
-    }
-
-    impl ToTypedQuerySpec for Foo {
-        type T = Bar;
-        fn fields() -> Bar {
-            Bar {
-                foo: Some("bla".to_string())
-            }
-        }
-    }
-
-    #[test]
-    fn it_works() {
-        let foo = Foo { bar: Some("bla".to_string()) };
-        let query_spec = QuerySpec::new(HashMap::new());
-
-        let result = TestService::find(&"foo", query_spec).expect("nooo").expect("nooo");
-        assert_eq!(foo, result);
-    }
-}
