d0e6333f257b21b5dd6ba1dcbe4392cca08c64d7
Blake Pettersson <blake.pettersson@gmail.com>
2017-04-05 00:29:44 +0200

    Make JsonApiId optional
    The reason we do this is because when a JSONAPI document is created, an
    id is not necessarily present. The From conversion traits does assume
    that an id will be present; any insecure conversions will be done using
    a custom TryFrom implementation.

--- jsonapi/src/data.rs
+++ jsonapi/src/data.rs
@@ -4,14 +4,14 @@ use params::JsonApiResource;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct JsonApiData<T> {
-    pub id: JsonApiId,
+    pub id: Option<JsonApiId>,
     #[serde(rename = "type")]
     pub lower_case_type: String,
     pub attributes: T,
 }
 
 impl<T> JsonApiData<T> {
-    pub fn new(id: JsonApiId, lower_case_type: String, attrs: T) -> JsonApiData<T> {
+    pub fn new(id: Option<JsonApiId>, lower_case_type: String, attrs: T) -> JsonApiData<T> {
         JsonApiData {
             id: id,
             lower_case_type: lower_case_type,
@@ -21,14 +21,26 @@ impl<T> JsonApiData<T> {
 }
 
 impl<'a, T> From<(T, &'a <T as JsonApiResource>::Params)> for JsonApiData<T::Attrs>
-    where T: ToJson,
-          T: JsonApiResource,
-          T::Attrs: From<(T, &'a <T as JsonApiResource>::Params)>
+    where T: ToJson + JsonApiResource,
+          T::Attrs: From<(T, &'a T::Params)>
 {
-    fn from(tuple: (T, &'a <T as JsonApiResource>::Params)) -> Self {
+    // In this case we know that there's going to be a Some(JsonApiId).
+    fn from(tuple: (T, &'a T::Params)) -> Self {
         let (model, params) = tuple;
-        JsonApiData::new(model.id(),
+        JsonApiData::new(Some(model.id()),
                          model.type_name(),
                          T::Attrs::from((model, params)))
+    }
+}
+
+impl<'a, T> From<T> for JsonApiData<T::Attrs>
+    where T: ToJson + JsonApiResource,
+          T::Params : 'a,
+          T::Attrs: for <'b> From<(T, &'b T::Params)>
+{
+    // In this case we know that there's going to be a Some(JsonApiId).
+    fn from(model: T) -> Self {
+        let params: T::Params = Default::default();
+        JsonApiData::new(Some(model.id()), model.type_name(), T::Attrs::from((model, &params)))
     }
 }
